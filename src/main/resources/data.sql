1cce9d7b7b62caaa7626b371578687fb45de40c0
Hector Javier <hectorjaviermella@gmail.com>
2023-03-28 18:12:18 -0300

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO edificio (id,nombre, domicilio)VALUES (1, "edificio1","direccion1");
+INSERT INTO edificio (id,nombre, domicilio)VALUES (2, "edificio2","direccion2");
+INSERT INTO edificio (id,nombre, domicilio)VALUES (3, "edificio3","direccion3");
+INSERT INTO edificio (id,nombre, domicilio)VALUES (4, "edificio4","direccion4");
+INSERT INTO edificio (id,nombre, domicilio)VALUES (5, "edificio5","direccion5");
+
+
+INSERT INTO dependencia(id,nombre, domicilio,edificio)VALUES (1, "dependencia1","domicilio1",1);
+INSERT INTO dependencia(id,nombre, domicilio,edificio)VALUES (2, "dependencia2","domicilio2",2);
+INSERT INTO dependencia(id,nombre, domicilio,edificio)VALUES (3, "dependencia3","domicilio3",3);
